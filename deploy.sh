47ff9c7503318a224d0fc52f67614a05e609028d
Danilo Sato <dtsato@thoughtworks.com>
2020-06-07 21:40:00 +0100

    Exercise 12

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -xe
 
+echo "Deploying pet-db..."
 kubectl apply -f kubernetes/mysql.yml --namespace default
-kubectl apply -f kubernetes/web.yml --namespace default
+
+IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
+PROJECT_ID=${GCLOUD_PROJECT_ID:-devops-workshop-123}
+echo "Deploying pet-web image version: $IMAGE_VERSION"
+
+cat kubernetes/web.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$IMAGE_VERSION/" | kubectl apply -f - --namespace default
